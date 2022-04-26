41aace5cb33468618aa2e4d47f1a095ad24c7859
polunwu <polunwu@gmail.com>
2022-04-26 20:59:57 +0800

    feat(List): update List type

--- src/03_list/List.type.ts
+++ src/03_list/List.type.ts
@@ -3,6 +3,9 @@ export type Index = number;
 export interface List {
   isEmpty(): boolean;
   count(): number;
+  clear(): void;
+  getItem(index: Index): Item;
   insertItem(index: Index, item: Item): void;
+  removeItem(index: Index): Item;
   locateItem(item: Item): Index;
 }
