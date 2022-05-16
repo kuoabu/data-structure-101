7d6a2999fa30d76b47156c1a3e6f2aca74499dd4
polunwu <polunwu@gmail.com>
2022-05-16 22:57:27 +0800

    feat(List): implementing SingleLinkedList

--- src/03_list/SingleLinkedList.ts
+++ src/03_list/SingleLinkedList.ts
@@ -1,19 +1,101 @@
 import * as Types from './List.type';
 
+class Node implements Types.Node {
+  value: number;
+  next: Types.Node | null;
+  constructor(item: number) {
+    this.value = item;
+    this.next = null;
+  }
+}
+
 export default class SingleLinkedList implements Types.List {
-  _count: number;
-  head: Types.Node | null;
+  protected _length: number;
+  protected _head: Types.Node | null;
 
   constructor() {
-    this._count = 0;
-    this.head = null;
+    this._length = 0;
+    this._head = null;
   }
 
   isEmpty(): boolean {
-    return this._count === 0;
+    return this._length === 0;
   }
 
   count(): number {
-    return this._count;
+    return this._length;
+  }
+
+  clear(): void {
+    this._head = null;
+  }
+
+  private _findNode(index: number): Types.Node | null {
+    let head = this._head;
+    let count = 0;
+    while (head) {
+      if (index === count) {
+        return head;
+      }
+      count++;
+      head = head.next;
+    }
+    return null;
+  }
+
+  getItem(index: number): number {
+    let found = this._findNode(index);
+    if (found) {
+      return found.value;
+    } else {
+      throw new Error('Item not found.');
+    }
+  }
+
+  insertItem(index: number, item: number): void {
+    let newNode = new Node(item);
+
+    if (index === 0) {
+      newNode.next = this._head;
+      this._head = newNode;
+    }
+
+    let prev = this._findNode(index - 1);
+    if (prev) {
+      newNode.next = prev.next;
+      prev.next = newNode;
+    } else {
+      throw new Error('Item not found.');
+    }
+  }
+
+  removeItem(index: number): number {
+    if (index === 0 && this._head) {
+      let item = this._head.value;
+      this._head = this._head.next;
+      return item;
+    }
+
+    let prev = this._findNode(index - 1);
+    if (prev && prev.next) {
+      let item = prev.next.value;
+      prev.next = prev.next.next;
+      return item;
+    } else {
+      throw new Error('Item not found.');
+    }
+  }
+
+  locateItem(item: number): number {
+    let head = this._head;
+    let count = 0;
+    while (head) {
+      if ((item = head.value)) {
+        return count;
+      }
+      count++;
+      head = head.next;
+    }
+    throw new Error('Item not found.');
   }
 }
